eefdcb1bbafed00ee79de1e7d25deaa90fdef3bd
Afsar Hussain <logicsoft152@gmail.com>
2026-08-20 12:16:39 +0530

    renew button correction and responsive change seat popup

--- src/app/shared/component/student-header/student-header.component.ts
+++ src/app/shared/component/student-header/student-header.component.ts
@@ -1,50 +1,3 @@
-// import { Component, HostListener, Output, EventEmitter } from '@angular/core';
-// import { Router, NavigationEnd } from '@angular/router';
-
-// @Component({
-//   selector: 'app-student-header',
-//   standalone: false,
-//   templateUrl: './student-header.component.html',
-//   styleUrl: './student-header.component.css'
-// })
-// export class StudentHeaderComponent {
-//   isMenuOpen = false;
-//   isNavbarHidden = false;
-//   isLoggedIn = false;
-//   private lastScrollTop = 0;
-
-//   constructor(private router: Router) {
-//     this.router.events.subscribe((event) => {
-//       if (event instanceof NavigationEnd) {
-//         this.isLoggedIn = !event.url.includes('/login');
-//       }
-//     });
-//   }
-
-//   @Output() openComplaint = new EventEmitter<void>();
-//   @HostListener('window:scroll', [])
-//   onWindowScroll() {
-//     const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-//     if (scrollTop > this.lastScrollTop && scrollTop > 100) {
-//       // Scrolling down → hide navbar
-//       this.isNavbarHidden = true;
-//     } else {
-//       // Scrolling up → show navbar
-//       this.isNavbarHidden = false;
-//     }
-
-//     this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-//   }
-//   toggleMenu() {
-//     this.isMenuOpen = !this.isMenuOpen;
-//   }
-
-//   logout() {
-//     this.isLoggedIn = false;
-//     this.router.navigate(['/login']);
-//   }
-// }
 
 import { Component, HostListener, Output, EventEmitter, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
@@ -145,7 +98,7 @@ export class StudentHeaderComponent implements OnInit {
   }
 
   renewPlan() {
-    if (!this.isPlanExpired) return;
+    if (!this.canRenew) return;
 
     const raw = sessionStorage.getItem('userdata');
     if (!raw) return;
